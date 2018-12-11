bb0db9e4716ef3d9ac97f3313d54b751e7d6b4c5
Kevin OConell <kevin.oconnell@verizon.com>
2018-12-11 09:37:35 -0500

    Lab7

---
+++ lab_input/input_ip.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Written by: Kevin O'Connell
+# Collect IP address information from a user and display it back to them.
+
+## Collect input from the user
+user_input = input ("Please enter an IPv4 IP address: ") # user_input stores the IPv4 IP address
+
+## Print out the input collected from the user
+print("You told me the IPv4 address is: " + user_input)
+
+## Collect input from the user
+vendor_name = input ("Please enter vendor name: ") # vendor_name stores the vendor name
